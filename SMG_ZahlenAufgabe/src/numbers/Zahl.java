86c36d8516f7fb30ab9b0c059557ef4efb2102c8
VChouliak <chouliak@hotmail.com>
2020-09-24 22:30:28 +0200

    Einfuegen von JavaDoc Kommentare zu Zahl.java

--- SMG_ZahlenAufgabe/src/numbers/Zahl.java
+++ SMG_ZahlenAufgabe/src/numbers/Zahl.java
@@ -1,16 +1,23 @@
 package numbers;
 
+/**
+ * Abstrakte Basisklasse für die vier Grundrechenarten: addieren, subtrahieren, multiplizieren und dividieren.
+ * Die Metoden sind in {@link numbers.Rational Rational} - Klasse in package {@link numbers numbers} definiert.
+ */
 public abstract class Zahl {
 	
-	 //Jede Zahl sollte mit den 4 Grundrechenarten bearbeitet werden können.	
+	/**Metode für die Addition von Zahlen.*/
 	 public abstract void add(Zahl a);
 	 
+	/**Metode für die Subtraktion von Zahlen.*/
 	 public abstract void sub(Zahl z);
 	 
+	/**Metode für die Multiplikation von Zahlen.*/
 	 public abstract void mul(Zahl z);
 	 
+	/**Metode für die Division von Zahlen.*/
 	 public abstract void div(Zahl z);	 
-	 
-	 //Anzeigen der Zahl sollte auch möglich sein.
+		 
+	 /**Metode zum ausgeben von Zahlen.*/
 	 public abstract void print();
 }
