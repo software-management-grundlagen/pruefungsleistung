6b6131c850d6b47cdbd787d745b4bb7482b2261b
VChouliak <chouliak@hotmail.com>
2020-09-26 12:18:47 +0200

    Einfuegen von logger zu main in App.java

--- SMG_ZahlenAufgabe/src/main/java/App.java
+++ SMG_ZahlenAufgabe/src/main/java/App.java
@@ -1,10 +1,22 @@
 import numbers.Rational;
 
+import ch.qos.logback.classic.Logger;
+
+import org.slf4j.LoggerFactory;
+
+
 public class App {
-    public static void main(String[] args) throws Exception {
+    private static final Logger logger 
+          = (Logger) LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) throws Exception {        
+       
+        logger.info("Main ist gestartet.");
+       
         System.out.println("\nHallo !!! mit diesem App wird die eingegebene Bruch gekuerzt !!!\n");
         new Rational().eingabe();
-        System.out.println("\nVielen Dank fuer die Benutzung und Tschuss!!!\n");
+        System.out.println("\nVielen Dank fuer die Benutzung und Aufwiedersehen!!!\n");
 
+        logger.info("Main ist beendet.");
     }
 }
